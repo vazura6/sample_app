a42af15844fce8f454de95539133023ab97ebfe6
Jozein <jobari2@googlemail.com>
2011-01-10 13:37:43 +0100

    Finished layout and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,13 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
   end
-
   def contact
     @title = "Contact"
   end
-
   def about
     @title = "About"
-  end
-  
+  end  
   def help
     @title = "Help"
   end
-
 end
